d4f3d44de8152c247603fd96467f751a4c7fbad1
hotsno <71658949+hotsno@users.noreply.github.com>
2022-07-15 23:13:36 -0700

    Testing colorama

--- utils/continue_watching.py
+++ utils/continue_watching.py
@@ -1,11 +1,14 @@
 import subprocess
 from colorama import Fore, Style
 import os
+import colorama
 
 from . import anilist_requests
 from . import mapper
 from . import offset
 
+
+colorama.init()
 g = Fore.GREEN
 re = Style.RESET_ALL
 
