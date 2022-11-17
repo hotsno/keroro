da0524143fab8f388fbdf3570472ce183da30f13
hotsno <71658949+hotsno@users.noreply.github.com>
2022-11-16 16:31:01 -0800

    Clean up

--- update.py
+++ update.py
@@ -1,24 +1,10 @@
-import os
-import sys
-
-import utils.anilist_requests
-import utils.config
-import utils.mapper
-
-file_path = sys.argv[1]
-(folder_path, file_name) = os.path.split(file_path)
-folder_map = utils.mapper.get_map()
-offset = 0
-print(folder_map)
-print(folder_path)
-print(file_name)
-if "offset" in folder_map[folder_path]:
-    offset = folder_map[folder_path]["offset"]
+import os, sys
+import utils.anilist_requests, utils.config, utils.mapper
 
 def get_progress(mediaId):
     return utils.anilist_requests.get_progress(mediaId)
 
-def update_progress(mediaId, progress):
+def update_progress(mediaId, progress, offset):
     episodes = []
     for file in sorted(os.listdir(folder_path)):
         if file.startswith('.'):
@@ -29,9 +15,16 @@ def update_progress(mediaId, progress):
         quit()
     utils.anilist_requests.update_progress(mediaId, watched_episode)
 
-for folder in folder_map:
-    if folder_path == folder:
-        mediaId = folder_map[folder]["anilist_id"]
-        progress = get_progress(mediaId)
-        update_progress(mediaId, progress)
-        break
+
+file_path = sys.argv[1]
+folder_path, file_name = os.path.split(file_path)
+folder_map = utils.mapper.get_map()
+if folder_path not in folder_map:
+    quit()
+
+mediaId = folder_map[folder_path]["anilist_id"]
+progress = get_progress(mediaId)
+offset = 0
+if 'offset' in folder_map[folder_path]:
+    offset = folder_map[folder_path]['offset']
+update_progress(mediaId, progress, offset)
